58b06abd2c8a48f64b5d47ae150c4103f658b8bc
Michael Sobolev <michaelsobolev@Michaels-MacBook-Pro.local>
2017-03-03 18:59:02 -0500

    forgot to remove require reload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-var reload = require('reload')
+// var reload = require('reload')
 
 // Get our API routes
 const api = require('./server/routes/api');
