52420d66243bdb48ec0847988d0badebd1b1a3d1
Michael Sobolev <michaelsobolev@Michaels-MacBook-Pro.local>
2017-03-03 19:57:58 -0500

    latest

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/Rx';
 @Injectable()
 export class DataService {
 
-  private _baseUrl = location.href.indexOf('localhost') >= 0 ? '/api/' : '';
+  private _baseUrl = location.href.indexOf('localhost') >= 0 ? '/api/' : '/api/';
 
   constructor(private http: Http) {}
 
