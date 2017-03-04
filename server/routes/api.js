23e9acb4ea87b55248b921d180d01bc4d9a5c777
Michael Sobolev <michaelsobolev@Michaels-MacBook-Pro.local>
2017-03-04 15:40:14 -0500

    access to db on route /api/Barrie

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var request = require("request");
 var parseString = require('xml2js').parseString;
+var pg = require('pg');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
@@ -9,10 +10,27 @@ router.get('/', (req, res) => {
 });
 
 router.get('/test', (req, res) => {
-    res.send({test: 'works'});
+    res.send({ test: 'works' });
 });
 
-router.get('/weathercast/:cityId',  function (req, res) {
+router.get('/Barrie', (req, res) => {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('SELECT * FROM city_weather', function (err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { 
+                //console.log(result.rows) 
+                res.send({ results: result.rows });
+             }
+        });
+    });
+    //res.send({ test: 'works' });
+});
+
+
+router.get('/weathercast/:cityId', function (req, res) {
     console.log('weathercast get route');
     request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/ON/" + req.params.cityId + "_e.xml", function (error, response, body) {
         var xml = body;
@@ -24,7 +42,7 @@ router.get('/weathercast/:cityId',  function (req, res) {
             //console.log(result);
             //res.send(result.siteData.currentConditions[0].temperature[0]._);
             res.send(result.siteData);
-            
+
         });
     });
 });
