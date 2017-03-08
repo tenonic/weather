46780ada7cc92a6365082460249d6cd64d9c7a74
Michael Sobolev <michaelsobolev@Michaels-MacBook-Pro.local>
2017-03-08 17:13:55 -0500

    some selecting, inserting

--- server/routes/api.js
+++ server/routes/api.js
@@ -31,92 +31,132 @@ router.get('/dbtest', (req, res) => {
 
 router.get('/dbtest/:provinceCode/:cityCode/:cityName', (req, res) => {
 
-
-
-    //todo: need to check if cityName and cityCode
-    request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/ON/" + req.params.cityCode + "_e.xml", function (error, response, body) {
-        var xml = body;
-
-        if (error) {
-
+    pg.connect(conString, (err, client, done) => {
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({ success: false, data: err });
         } else {
+            client.query("SELECT * FROM city_weather WHERE city_code = $1",[req.params.cityCode], function (q_err, q_result) {
+                if (q_result.rowCount > 0) {
+                    var exp_date = q_result.rows[0].expiry_date;
+                    if (new Date() > exp_date) {
+                        console.log('time to update')
 
-            //todo: take out parsing into helper module
-            parseString(xml, function (err, result) {
-                //console.log(result);
-                //res.send(result.siteData);
-
-                //todo: take out the db actions to a db module
-                pg.connect(conString, (err, client, done) => {
-                    if (err) {
-                        done();
-                        console.log(err);
-                        return res.status(500).json({ success: false, data: err });
                     } else {
-                        client.query('INSERT INTO city_weather(city_name, city_code, province_code, conditions) values($1, $2, $3, $4)',
-                            [req.params.cityName, req.params.cityCode, req.params.provinceCode, result.siteData]);
-                        res.status(200);
-                        res.send('inserted');
+                        console.log('not time to update yet');
                     }
-                });
+                    res.send(q_result.rows);
+                } else {
+                    console.log('not found');
+                    res.send('no record found');
+                }
             });
         }
-
-
     });
-
-
-
 });
 
+    router.get('/dbinsert/:provinceCode/:cityCode/:cityName', (req, res) => {
 
-router.get('/Barrie', (req, res) => {
+        pg.connect(conString, (err, client, done) => {
+            if (err) {
+                done();
+                console.log(err);
+                return res.status(500).json({ success: false, data: err });
+            } else {
+                //todo: need to check if cityName and cityCode
+                request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/" + req.params.provinceCode + "/" + req.params.cityCode + "_e.xml", function (error, response, body) {
+                    console.log('right');
+                    var xml = body;
 
-    console.log('barrie get');
+                    if (error) {
+                        console.log('error on the request');
 
-    pg.connect(conString, function (err, client, done) {
-        client.query('SELECT * FROM city_weather', function (err, result) {
-            done();
-            if (err)
-            { console.error(err); res.send("Error " + err); }
-            else {
-                //console.log(result.rows) 
-                res.send({ results: result.rows });
+                    } else {
+
+                        //todo: take out parsing into helper module
+                        parseString(xml, function (err, result) {
+
+                            var curDate = new Date();
+                            var expiryDate = new Date(curDate);
+                            expiryDate.setMinutes(curDate.getMinutes() + 5);
+                            client.query('INSERT INTO city_weather(city_name, city_code, province_code, conditions, created_date, modified_date, expiry_date) values($1, $2, $3, $4, $5, $6, $7)',
+                                [req.params.cityName, req.params.cityCode, req.params.provinceCode, result.siteData, curDate, curDate, expiryDate]);
+                            res.status(200);
+                            res.send('inserted');
+
+                            //todo: take out the db actions to a db module
+
+                            // const results = [];
+                            // const query = client.query("SELECT * FROM city_weather WHERE city_code = 's0000415'");
+                            // // Stream results back one row at a time
+                            // query.on('row', (row) => {
+                            //     results.push(row);
+                            // });
+                            // // After all data is returned, close connection and return results
+                            // query.on('end', () => {
+                            //     done();
+                            //     res.send(results);
+                            // });
+
+                        });
+                    }
+                });
             }
         });
+
+
+
     });
-    //res.send({ test: 'works' });
-});
 
 
-router.get('/weathercast/:cityId', function (req, res) {
-    console.log('weathercast city route');
-    request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/ON/" + req.params.cityId + "_e.xml", function (error, response, body) {
-        var xml = body;
+    router.get('/Barrie', (req, res) => {
 
-        // var json = parser.toJson(xml);
-        // res.send(json);
+        console.log('barrie get');
 
-        parseString(xml, function (err, result) {
-            //console.log(result);
-            //res.send(result.siteData.currentConditions[0].temperature[0]._);
-            res.send(result.siteData);
+        pg.connect(conString, function (err, client, done) {
+            client.query('SELECT * FROM city_weather', function (err, result) {
+                done();
+                if (err)
+                { console.error(err); res.send("Error " + err); }
+                else {
+                    //console.log(result.rows) 
+                    res.send({ results: result.rows });
+                }
+            });
+        });
+        //res.send({ test: 'works' });
+    });
 
+
+    router.get('/weathercast/:cityId', function (req, res) {
+        console.log('weathercast city route');
+        request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/ON/" + req.params.cityId + "_e.xml", function (error, response, body) {
+            var xml = body;
+
+            // var json = parser.toJson(xml);
+            // res.send(json);
+
+            parseString(xml, function (err, result) {
+                //console.log(result);
+                //res.send(result.siteData.currentConditions[0].temperature[0]._);
+                res.send(result.siteData);
+
+            });
         });
     });
-});
 
-router.get('/weathercast/:provinceCode/:cityCode', function (req, res) {
-    console.log('weathercast province / city route');
-    request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/" + req.params.provinceCode + "/" + req.params.cityCode + "_e.xml", function (error, response, body) {
-        var xml = body;
+    router.get('/weathercast/:provinceCode/:cityCode', function (req, res) {
+        console.log('weathercast province / city route');
+        request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/" + req.params.provinceCode + "/" + req.params.cityCode + "_e.xml", function (error, response, body) {
+            var xml = body;
 
-        parseString(xml, function (err, result) {
-            console.log(result);
-            res.send(result.siteData);
+            parseString(xml, function (err, result) {
+                console.log(result);
+                res.send(result.siteData);
 
+            });
         });
     });
-});
 
-module.exports = router;
+    module.exports = router;
