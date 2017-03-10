0d76073847993a9b038b30ec67412548f7f05c3b
Michael Sobolev <michaelsobolev@Michaels-MacBook-Pro.local>
2017-03-10 17:05:17 -0500

    not sure what it is

--- server/routes/api.js
+++ server/routes/api.js
@@ -39,32 +39,58 @@ router.get('/dbtest/:provinceCode/:cityCode/:cityName', (req, res) => {
         } else {
 
             //todo: add cityName, provinceCode params
-            client.query("SELECT * FROM city_weather WHERE city_code = $1", [req.params.cityCode], function (q_err, q_result) {
-                if (q_result.rowCount > 0) {
-                    var exp_date = q_result.rows[0].expiry_date;
-                    if (new Date() > exp_date) {
-                        var id_upd = q_result.rows[0].id;
-                        console.log('time to update');
+            client.query("SELECT * FROM city_weather WHERE city_code = $1 AND province_code = $2 AND city_name = $3",
+                [req.params.cityCode, req.params.provinceCode, req.params.cityName], function (q_err, q_result) {
+                    if (q_result.rowCount > 0) {
+                        console.log('record found');
+                        var exp_date = q_result.rows[0].expiry_date;
+                        if (new Date() > exp_date) {
+                            var id_upd = q_result.rows[0].id;
+                            console.log('time to update');
 
-                        //todo: update other columns
+                            //todo: update other columns
 
-                        var curDate = new Date();
-                        var new_exp_date = new Date(curDate.setMinutes(curDate.getMinutes() + 5));
+                            var curDate = new Date();
+                            var new_exp_date = new Date(curDate.setMinutes(curDate.getMinutes() + 5));
 
-                        client.query("UPDATE city_weather SET expiry_date=($1)", [new_exp_date], function (q_er2, q_result2) {
-                            res.send(q_result2.rows);
-                        });
-                        
+                            client.query("UPDATE city_weather SET expiry_date=($1)", [new_exp_date], function (q_er2, q_result2) {
+                                res.send(q_result2);
+                            });
+
+                        } else {
+                            console.log('not time to update yet');
+                            //rows[0] ?
+                            res.send(q_result.rows);
+                        }
+
                     } else {
-                        console.log('not time to update yet');
-                        res.send(q_result.rows);
-                    }
+                        console.log('no records found, inserting')
+                        //insert record from the weather api
+                        request("http://dd.weatheroffice.ec.gc.ca/citypage_weather/xml/" + req.params.provinceCode + "/" + req.params.cityCode + "_e.xml", function (error, response, body) {
 
-                } else {
-                    console.log('not found');
-                    res.send('no record found');
-                }
-            });
+                            var xml = body;
+
+                            if (error) {
+                                console.log('error on the request');
+
+                            } else {
+
+                                //todo: take out parsing into helper module
+                                parseString(xml, function (err, json) {
+                                    //console.log(json);
+                                    var curDate = new Date();
+                                    var expiryDate = new Date(curDate);
+                                    expiryDate.setMinutes(curDate.getMinutes() + 5);
+                                    client.query('INSERT INTO city_weather(city_name, city_code, province_code, conditions, created_date, modified_date, expiry_date)'
+                                        + 'values($1, $2, $3, $4, $5, $6, $7)',
+                                        [req.params.cityName, req.params.cityCode, req.params.provinceCode, json.siteData, curDate, curDate, expiryDate]);
+                                    res.status(200);
+                                    res.send(json.siteData);
+                                });
+                            }
+                        });
+                    }
+                });
         }
     });
 });
@@ -89,14 +115,14 @@ router.get('/dbinsert/:provinceCode/:cityCode/:cityName', (req, res) => {
 
                     //todo: take out parsing into helper module
                     parseString(xml, function (err, result) {
-
+                        //console.log(result);
                         var curDate = new Date();
                         var expiryDate = new Date(curDate);
                         expiryDate.setMinutes(curDate.getMinutes() + 5);
                         client.query('INSERT INTO city_weather(city_name, city_code, province_code, conditions, created_date, modified_date, expiry_date) values($1, $2, $3, $4, $5, $6, $7)',
                             [req.params.cityName, req.params.cityCode, req.params.provinceCode, result.siteData, curDate, curDate, expiryDate]);
                         res.status(200);
-                        res.send('inserted');
+                        res.send(result.siteData);
 
                         //todo: take out the db actions to a db module
 
